19c691b5eb50776a206fd4e2e3229862bb9c1e56
Anuj Singh <kumaranuj1303@gmail.com>
2022-10-21 14:50:22 +0530

    Add icons screen

---
+++ app/src/main/java/dev/anuj/iconfinder/ui/icons/IconsAdapter.kt
@@ -0,0 +1,38 @@
+package dev.anuj.iconfinder.ui.icons
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import coil.load
+import dev.anuj.iconfinder.R
+import dev.anuj.iconfinder.data.Icon
+import dev.anuj.iconfinder.databinding.LayoutIconItemBinding
+
+class IconsAdapter(
+    private val icons: List<Icon>,
+    private val onDownloadClick: (Icon) -> Unit
+) : RecyclerView.Adapter<IconsAdapter.ViewHolder>() {
+
+    inner class ViewHolder(val binding: LayoutIconItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutIconItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        with (holder.binding) {
+            val icon = icons[position]
+            imageViewIcon.load(icon.raster_sizes.find { it.size == 128 }?.formats?.get(0)?.preview_url)
+            textViewIconId.text = icon.icon_id.toString()
+            textViewIconTags.text = icon.tags.joinToString(", ")
+            imageViewIconDownload.setImageResource(if (icon.is_premium) R.drawable.baseline_attach_money else R.drawable.baseline_cloud_download)
+
+            if (!icon.is_premium) imageViewIconDownload.setOnClickListener {
+                onDownloadClick(icon)
+            }
+        }
+    }
+
+    override fun getItemCount() = icons.size
+}
--- app/src/main/java/dev/anuj/iconfinder/ui/icons/IconsViewModel.kt
+++ app/src/main/java/dev/anuj/iconfinder/ui/icons/IconsViewModel.kt
@@ -1,24 +1,25 @@
-package dev.anuj.iconfinder.ui.iconset
+package dev.anuj.iconfinder.ui.icons
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dev.anuj.iconfinder.data.Iconset
+import dev.anuj.iconfinder.data.Icon
 import dev.anuj.iconfinder.network.ApiService
 import javax.inject.Inject
 import kotlinx.coroutines.launch
 
 @HiltViewModel
-class IconsetViewModel @Inject constructor(
+class IconsViewModel @Inject constructor(
     private val api: ApiService
-) : ViewModel() {
+): ViewModel() {
 
-    val iconsets = MutableLiveData<List<Iconset>>()
+    val icons = MutableLiveData<List<Icon>>()
 
-    fun getIconsets(categoryId: String) {
+
+    fun getIcons(iconsetId: Int) {
         viewModelScope.launch {
-            iconsets.value = api.getIconSets(categoryId).iconsets
+            icons.value = api.getIcons(iconsetId).icons
         }
     }
 }
